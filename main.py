5e1fb0ff9bea86f34ebbeba88fe439f491f8293a
arndri <hardwarebuild041100@gmail.com>
2023-03-07 12:24:44 +0700

    commit

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
 from tkinter import *
+from tkinter import messagebox
+import random
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+    pass_lists=[]
+    nr_letters = random.randint(7,10)
+    nr_symbols = random.randint(2,5)
+    nr_numbers = random.randint(1,5)
 
+    totalnr=nr_letters+nr_numbers+nr_symbols
+    for n in range(0,nr_letters):
+        pass_lists.append(random.choice(letters))
+    for n in range(0,nr_symbols):
+        pass_lists.append(random.choice(symbols))
+    for n in range(0,nr_numbers):
+        pass_lists.append(random.choice(numbers))
+    random.shuffle(pass_lists)
+    passw = "".join(pass_lists)
+    in3.insert(0,passw)
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def to_txt():
     website = in1.get()
     email = in2.get()
     password = in3.get()
-    txt_file = open("data.txt","a")
-    txt_file.write(f"{website},{email},{password} \n")
-    txt_file.close()
+    
+    if len(website) == 0 or len(password) == 0 :
+        messagebox.showinfo(title="Error",message="Please enter the email and password")
+    else:
+        swt = messagebox.askokcancel(title=website,message=f"The details entered : \nEmail: {email}\nPassword: {password}\nIs it correct ?")
+        if swt:
+            txt_file = open("data.txt","a")
+            txt_file.write(f"{website},{email},{password} \n")
+            txt_file.close()
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -28,7 +54,7 @@ label2 = Label(text="Email/Username:")
 in2 = Entry(width=35)
 label3 = Label(text="Password:")
 in3 = Entry(width=35)
-generate_pass_btn = Button(text="Generate Password",width=20)
+generate_pass_btn = Button(text="Generate Password",width=20,command=generate)
 add_btn = Button(text="Add",width=20,command=to_txt)
 
 
