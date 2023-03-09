32e5e42309a66797d0d959cf9993af56fa6a8cde
arndri <hardwarebuild041100@gmail.com>
2023-03-09 19:33:32 +0700

    add search feature

--- main.py
+++ main.py
@@ -2,6 +2,27 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import json
+# ---------------------------- SEARCH WEBSITE ------------------------------- #
+
+def search():
+    value = in1.get()
+    email = ""
+    password = ""
+    try:
+        with open("data.json","r") as f:
+            temp1_data = json.load(f)
+    except FileNotFoundError:
+        messagebox.showerror(title="File Not Found",message="No Data File Found")
+    else:
+        if value in temp1_data:
+            email = temp1_data[value]["email"]
+            password = temp1_data[value]["password"]
+            messagebox.showinfo(title="Found !",message=f"Website : {value}\nEmail : {email}\nPassword : {password}")
+        else:
+            messagebox.showinfo(title="Data Not Found",message="The data you are looking for is not in the file")
+            in1.delete(0,END)
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -80,7 +101,7 @@ label3 = Label(text="Password:")
 in3 = Entry(width=35)
 generate_pass_btn = Button(text="Generate Password",width=20,command=generate)
 add_btn = Button(text="Add",width=20,command=to_txt)
-
+src_btn = Button(text="Search",width=20,command=search)
 
 
 label1.grid(column=0,row=1)
@@ -93,5 +114,5 @@ in2.insert(0,"andri@gmail.com")
 in3.grid(column=1,row=3,columnspan=2)
 generate_pass_btn.grid(column=2,row=4)
 add_btn.grid(column=1,row=4)
-
+src_btn.grid(column=1,row=5,columnspan=2)
 window.mainloop()
